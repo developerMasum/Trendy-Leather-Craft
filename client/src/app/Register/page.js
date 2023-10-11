79f23db99c3c0f88ea162bd9852f1952b1206ae0
programmer-kabir <programmerkabirr@gmail.com>
2023-10-11 22:55:43 +0600

    few change

--- client/src/app/Register/page.js
+++ client/src/app/Register/page.js
@@ -8,13 +8,13 @@ const RegisterPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [isInputFocused, setInputFocused] = useState(false);
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
+  const [passwordConditions, setPasswordConditions] = useState({
+    minLength: false,
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    specialChar: false,
+  });
   const {
     handleSubmit,
     control,
@@ -23,21 +23,50 @@ const RegisterPage = () => {
     formState: { errors },
   } = useForm();
   const watchedFields = watch();
-  const allFieldsFilled =
-    watchedFields.firstName?.trim().length &&
-    watchedFields.lastName?.trim().length &&
-    watchedFields.number?.trim().length &&
-    watchedFields.email?.trim().length &&
-    watchedFields.password?.trim().length &&
-    watchedFields.confirmPassword?.trim().length;
+  const password = watch("password");
+  // Password Validity
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
 
+    setPasswordConditions({
+      minLength: value.length >= 8,
+      uppercase: /[A-Z]/.test(value),
+      lowercase: /[a-z]/.test(value),
+      number: /[0-9]/.test(value),
+      specialChar: /[!@#$&*]/.test(value),
+    });
+  };
+    // password show
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  // Confirm password show
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  // Button Checkbox 
   const isAccepted = watchedFields.acceptance === "accept";
-  const isButtonDisabled = !(allFieldsFilled && isAccepted);
+  const isButtonDisabled = !isAccepted;
+  // From
+  const onSubmit = (data) => {
+    console.log(data);
+    if (data.password !== data.confirmPassword) {
+      console.error("Passwords do not match!");
+      return;
+    }
+  };
+
   return (
     <div className="px-6 py-20 w-2/3 mx-auto">
       <h2 className="text-3xl text-center">REGISTRATION</h2>
-      <p className="text-center pt-2 text-gray-700">Create an account and Register yourself as Trendy-Leather-Craft Club member! Trendy-Leather-Craft club members can enjoy exclusive benefits.</p>
-      <form className=" space-y-4">
+      <p className="text-center pt-2 text-gray-700">
+        Create an account and Register yourself as Trendy-Leather-Craft Club
+        member! Trendy-Leather-Craft club members can enjoy exclusive benefits.
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)} className=" space-y-4">
+        {/* First name and last name */}
         <div className="grid grid-cols-2 gap-7 mt-7">
           <div className="">
             <label className="text-sm flex items-center gap-2">
@@ -46,6 +75,7 @@ const RegisterPage = () => {
             <input
               id="firstName"
               type="text"
+              name="firstName"
               placeholder="First name"
               {...register("firstName", { required: true })}
               className="w-full py-1 rounded px-2 text-lg  border outline-none border-gray-600 text-black"
@@ -58,12 +88,14 @@ const RegisterPage = () => {
             <input
               id="lastName"
               type="text"
+              name="lastName"
               {...register("lastName", { required: true })}
               placeholder="Last name"
               className="w-full text-lg py-1 px-2 rounded  border outline-none border-gray-600 text-black"
             />
           </div>
         </div>
+        {/* Mobile */}
         <div className="">
           <label className="text-sm flex items-center gap-2">
             Mobile No <span className="text-red-700">*</span>{" "}
@@ -71,11 +103,13 @@ const RegisterPage = () => {
           <input
             id="number"
             type="text"
+            name="number"
             {...register("number", { required: true })}
             placeholder="number"
             className="w-full text-lg py-1 px-2 rounded  border outline-none border-gray-600 text-black"
           />
-        </div>  
+        </div>
+        {/* Email */}
         <div className="">
           <label className="text-sm flex items-center gap-2">
             Email <span className="text-red-700">*</span>{" "}
@@ -83,6 +117,7 @@ const RegisterPage = () => {
           <input
             id="email"
             type="email"
+            name="email"
             {...register("email", { required: true })}
             placeholder="Your Email Here"
             className="w-full text-lg py-1 px-2 rounded  border outline-none border-gray-600 text-black"
@@ -96,6 +131,7 @@ const RegisterPage = () => {
           <div className="flex justify-between py-1 px-2 rounded items-center border border-gray-600">
             <input
               id="password"
+              name="password"
               type={showPassword ? "text" : "password"}
               placeholder="Your password Here"
               {...register("password", {
@@ -106,7 +142,7 @@ const RegisterPage = () => {
               })}
               className="w-full    outline-none  text-black"
               onFocus={() => setInputFocused(true)}
-              onBlur={() => setInputFocused(false)}
+              onChange={handlePasswordChange}
             />
             <span className="px-4">
               {showPassword ? (
@@ -125,11 +161,27 @@ const RegisterPage = () => {
             </span>
           </div>
           {isInputFocused && (
-            <span className="text-gray-500 py-2 text-sm">
-              The password should be at least 8 characters long and must contain
-              at least one upper case letter, one lower case letter, a number
-              and a special character(!, @, #, $, %, &, *)
-            </span>
+            <div className="text-gray-500 py-2 text-sm">
+              <div>
+                {passwordConditions.minLength ? "✅" : "❌"} At least 8
+                characters long
+              </div>
+              <div>
+                {passwordConditions.uppercase ? "✅" : "❌"} Contains an
+                uppercase letter
+              </div>
+              <div>
+                {passwordConditions.lowercase ? "✅" : "❌"} Contains a
+                lowercase letter
+              </div>
+              <div>
+                {passwordConditions.number ? "✅" : "❌"} Contains a number
+              </div>
+              <div>
+                {passwordConditions.specialChar ? "✅" : "❌"} Contains a
+                special character(!, @, #, $, %, &, *)
+              </div>
+            </div>
           )}
         </div>
         {/* Confirm Password */}
@@ -140,13 +192,13 @@ const RegisterPage = () => {
           <div className="flex justify-between py-1 px-2 rounded items-center border border-gray-600">
             <input
               id="confirmPassword"
+              name="confirmPassword"
               type={showConfirmPassword ? "text" : "password"}
-              placeholder="Your password Here"
+              placeholder="Your confirm password Here"
               {...register("confirmPassword", {
                 required: true,
-                minLength: 8,
-                maxLength: 18,
-                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}/,
+                validate: (value) =>
+                  value === password || "Passwords do not match",
               })}
               className="w-full  outline-none  text-black"
             />
@@ -166,6 +218,7 @@ const RegisterPage = () => {
               )}
             </span>
           </div>
+          {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
         </div>
         {/* Radio */}
         <div className=" ">
@@ -177,9 +230,14 @@ const RegisterPage = () => {
               name="acceptance"
               className="mr-2"
             />
-            I accept <Link href='' className="text-blue-600 ml-1 hover:underline font-semibold">Terms and Condition</Link>
+            I accept{" "}
+            <Link
+              href=""
+              className="text-blue-600 ml-1 hover:underline font-semibold"
+            >
+              Terms and Condition
+            </Link>
           </label>
-
         </div>
         <button
           disabled={isButtonDisabled}
@@ -193,7 +251,6 @@ const RegisterPage = () => {
           Create an Account
         </button>
       </form>
-      
     </div>
   );
 };
